0f0bff548cf50ee646cf77fd1144c5b3ff8816f1
lucas <lucasecp7@gmail.com>
2021-03-23 22:52:49 -0300

    url c

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const axiosConfig = axios.create({
-  baseURL: 'https://app-api-dashboard.herokuapp.com'
+  baseURL: 'https://app-dashboardapi.herokuapp.com'
 });
 
 export default axiosConfig;
